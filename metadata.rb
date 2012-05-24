4cacac0f4d53ffd959503696f0030b545891fe0b
Dan Buch <d.buch@modcloth.com>
2012-05-24 11:34:00 -0400

    Switching readme to markdown per foodcritic, bumping version minorly

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,6 @@ maintainer       "RocketLabs Development"
 maintainer_email "info@rocketlabsdev.com"
 license          "All rights reserved"
 description      "Installs/Configures gitolite"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 depends          "git"
-version          "0.0.2"
+version          "0.0.3"
